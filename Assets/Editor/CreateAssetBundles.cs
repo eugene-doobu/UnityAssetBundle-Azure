ef9001ab2a4d3abb9f124f0ef1ae695da3d782b9
dn-soft <58686228+eugene-doobu@users.noreply.github.com>
2021-10-13 13:19:39 +0900

    Add BaseAssets and Editor script
    AssetBundle 생성 스크립트 추가
    
    추가 에셋
    Newtonsoft.Json-for-Unity
    Unity3D-LogCat-extension

---
+++ Assets/Editor/CreateAssetBundles.cs
@@ -0,0 +1,103 @@
+﻿using UnityEditor;
+using System.IO;
+using UnityEngine;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+
+[Serializable]
+public class AssetBundleHash
+{
+    [JsonProperty("name")] public string name;
+    [JsonProperty("hash")] public string hash;
+
+    public AssetBundleHash(string name, string hash)
+    {
+        this.name = name;
+        this.hash = hash;
+    }
+}
+
+[Serializable]
+public class AssetBundleHashs
+{
+    [JsonProperty("version")] public string version;
+    [JsonProperty("target")] public string target;
+    [JsonProperty("hashs")] public List<AssetBundleHash> hashs = new List<AssetBundleHash>();
+}
+
+public class CreateAssetBundles
+{
+    #region MenuItems
+    [MenuItem("AssetBundle/Build AssetBundles Window(Test)")]
+    static void BuildAllAssetBundleWindow() =>
+        BuildAllAssetBundles(BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+
+    [MenuItem("AssetBundle/Build AssetBundles Android")]
+    static void BuildAllAssetBundleAndroid() =>
+        BuildAllAssetBundles(BuildAssetBundleOptions.None, BuildTarget.Android);
+
+    [MenuItem("AssetBundle/Make AssetBundleManager Window(Test)")]
+    static void MakeAssetBundleManagerWindow() =>
+        MakeAssetBundleManager(BuildTarget.StandaloneWindows);
+
+    [MenuItem("AssetBundle/Make AssetBundleManager Android")]
+    static void MakeAssetBundleManagerAndroid() =>
+        MakeAssetBundleManager(BuildTarget.Android);
+    #endregion
+
+    static string assetBundleDirectory = "Assets/AssetBundles";
+
+    static void BuildAllAssetBundles(BuildAssetBundleOptions option, BuildTarget target) { 
+        // 에셋번들 저장
+        if (!Directory.Exists(assetBundleDirectory)) { 
+            Directory.CreateDirectory(assetBundleDirectory);
+        } 
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, option, target);
+    }
+
+    static void MakeAssetBundleManager(BuildTarget target)
+    {
+        // 에셋번들 매니저 생성
+        var bundleManager = new AssetBundleHashs();
+
+        bundleManager.version = Application.version;
+        bundleManager.target = GetTargetText(target);
+
+        Debug.Log("Version: " + bundleManager.version);
+        Debug.Log("Target : " + bundleManager.target);
+
+        // 번들목록 저장하기
+        const int lenAssetFolderName = 6;
+        string assetBundlesPath = assetBundleDirectory.Substring(lenAssetFolderName) + "/AssetBundles";
+        AssetBundle assetBundle = AssetBundle.LoadFromFile(Application.dataPath + assetBundlesPath);
+        var manifest = assetBundle.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
+
+        foreach (var bundle in manifest.GetAllAssetBundles())
+        {
+            Hash128 characterHash = manifest.GetAssetBundleHash(bundle);
+            bundleManager.hashs.Add(new AssetBundleHash(bundle, characterHash.ToString()));
+        }
+
+        File.WriteAllText(assetBundleDirectory + "/AssetBundleManager.json", JsonConvert.SerializeObject(bundleManager));
+        Debug.Log(assetBundleDirectory + "/AssetBundleManager.json");
+    }
+
+    #region Utilities
+    static string GetTargetText(BuildTarget target)
+    {
+        switch (target)
+        {
+            case BuildTarget.StandaloneWindows:
+                return "Window";
+            case BuildTarget.Android:
+                return "Android";
+            case BuildTarget.iOS:
+                return "iOS";
+            default:
+                return "Unknown";
+        }
+    }
+    #endregion
+}
